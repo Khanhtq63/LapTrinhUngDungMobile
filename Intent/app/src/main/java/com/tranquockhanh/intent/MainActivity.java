b2d04bf884f27ecf146f8a51072299093c86b069
Khanhtq63 <sitanred1234@gmail.com>
2023-10-25 11:13:35 +0700

    update

---
+++ Intent/app/src/main/java/com/tranquockhanh/intent/MainActivity.java
@@ -0,0 +1,21 @@
+package com.tranquockhanh.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void DangNhap(View v){
+        Intent iDangNhap = new Intent(this,LoginActivity.class);
+        startActivity(iDangNhap);
+    }
+}
