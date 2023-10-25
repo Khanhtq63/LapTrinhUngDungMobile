dd81efbaa128416e508face87bf6a80677c16628
Khanhtq63 <sitanred1234@gmail.com>
2023-10-25 11:35:33 +0700

    update

--- Intent/app/src/main/java/com/tranquockhanh/intent/HomeActivity.java
+++ Intent/app/src/main/java/com/tranquockhanh/intent/HomeActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -12,6 +13,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        TextView tvUser = (TextView) findViewById(R.id.tvUserName);
+        String userName = getIntent().getStringExtra("userName");
+        tvUser.setText(userName);
     }
 
 
--- Intent/app/src/main/java/com/tranquockhanh/intent/LoginActivity.java
+++ Intent/app/src/main/java/com/tranquockhanh/intent/LoginActivity.java
@@ -1,8 +1,12 @@
 package com.tranquockhanh.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void ChuyenSangHome(View v)
+    {
+        EditText edtUser = (EditText) findViewById(R.id.edtUserName);
+        EditText edtPass = (EditText) findViewById(R.id.edtPass);
+        EditText edtEmail = (EditText) findViewById(R.id.edtMail);
+
+        String userName = edtUser.getText().toString();
+        String userPass = edtPass.getText().toString();
+        String userMail = edtEmail.getText().toString();
+        if(kiemTra(userName,userPass,userMail)){
+            Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
+            intent.putExtra("userName",userName);
+            startActivity(intent);
+        } else {
+            Toast.makeText(LoginActivity.this,"Thông tin không chính xác",Toast.LENGTH_SHORT).show();
+        }
+    }
+    private boolean kiemTra(String userName,String password, String email){
+        return "tranquockhanh".equals(userName) && "98765432".equals(password) && "khanh.tq.63cntt@ntu.edu.vn".equals(email);
     }
 }
